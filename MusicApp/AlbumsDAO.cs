3cf3f9bea341200122cabfa3bd7bfc9fe6ccb726
DeadAnjali <anjalimahere15@gmail.com>
2024-04-25 03:10:09 +0530

    get-data-from-mysql-server

--- MusicApp/AlbumsDAO.cs
+++ MusicApp/AlbumsDAO.cs
@@ -16,7 +16,24 @@ namespace MusicApp
             connection.Open();
 
             //sequel statements
-
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM ALBUM",connection);
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Album a = new Album
+                    {
+                        ID = reader.GetInt32(0),
+                        AlbumName = reader.GetString(1),
+                        ArtistName = reader.GetString(2),
+                        Year = reader.GetInt32(3),
+                        imageURL = reader.GetString(4),
+                        Description = reader.GetString(5)
+                    };
+                    returnThese.Add(a);
+                }
+            }
+            connection.Close();
             return returnThese;
         }
     }
