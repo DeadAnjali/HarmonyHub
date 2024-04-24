ec7c0e98fa48b9db67733255f70020b644499b8e
DeadAnjali <anjalimahere15@gmail.com>
2024-04-25 01:58:13 +0530

    album class

---
+++ MusicApp/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp
+{
+    internal class Album
+    {
+        public int ID { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public int Year { get; set; }
+        public string imageURL { get; set; }
+        public string Description { get; set; }
+
+        //add list of classes
+    }
+}
